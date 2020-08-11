937989882b5a62e954dfd51e26abf571b2f70fe1
magurovS <69201987+magurovS@users.noreply.github.com>
2020-08-11 23:04:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-// var http = require('http')
-// var fs = require('fs')
-
-// var server = http.createServer(function(req, res) {
-// 	console.log(req.url)
-// 	if (req.url === '/index' || req.url === '/'){
-// 		res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'})
-// 		fs.createReadStream(__dirname + '/index.html').pipe(res)
-// 	} 
-	
-// })
-// server.listen(3000, "127.0.0.1")
-
 const express = require('express');
 const app = express();
 app.use('/', express.static(__dirname + '/'))
